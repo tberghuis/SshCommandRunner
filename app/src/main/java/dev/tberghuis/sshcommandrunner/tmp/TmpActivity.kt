e5e43e053465d1fed103907696fa3f0b95a8c26d
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-09-16 09:52:08 +1000

    .

---
+++ app/src/main/java/dev/tberghuis/sshcommandrunner/tmp/TmpActivity.kt
@@ -0,0 +1,33 @@
+package dev.tberghuis.sshcommandrunner.tmp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import dev.tberghuis.sshcommandrunner.ui.theme.SshCommandRunnerTheme
+
+class TmpActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      SshCommandRunnerTheme {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+          TmpScreen()
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun TmpScreen() {
+  Column {
+    Text("hello tmp screen")
+  }
+}
