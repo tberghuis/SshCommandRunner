8831aab8347bcfd3f7af68c8d3c2b2d11863c37f
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-09-16 09:40:49 +1000

    .

---
+++ app/src/main/java/dev/tberghuis/sshcommandrunner/tmp/SshService.kt
@@ -0,0 +1,13 @@
+package dev.tberghuis.sshcommandrunner.tmp
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class SshService : Service(){
+  override fun onBind(intent: Intent?): IBinder? {
+    TODO("Not yet implemented")
+  }
+
+
+}
