50b2e9a7cf60cd1e5a9fbcef06b832c9a0adc542
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-09-19 08:16:14 +1000

    .

--- app/src/main/java/dev/tberghuis/sshcommandrunner/tmp/SshController.kt
+++ app/src/main/java/dev/tberghuis/sshcommandrunner/tmp/SshController.kt
@@ -2,12 +2,15 @@ package dev.tberghuis.sshcommandrunner.tmp
 
 import dev.tberghuis.sshcommandrunner.BuildConfig
 import dev.tberghuis.sshcommandrunner.util.logd
+import java.net.InetSocketAddress
+import java.net.ServerSocket
 import java.util.concurrent.TimeUnit
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
 import net.schmizz.sshj.SSHClient
 import net.schmizz.sshj.common.IOUtils
+import net.schmizz.sshj.connection.channel.direct.Parameters
 import net.schmizz.sshj.transport.verification.PromiscuousVerifier
 
 
@@ -24,7 +27,8 @@ class SshController(private val scope: CoroutineScope) {
     logd("SshController run")
 
     scope.launch(IO) {
-      exampleExec()
+//      exampleExec()
+      exampleLocalPF()
     }
 
   }
@@ -44,4 +48,26 @@ class SshController(private val scope: CoroutineScope) {
     val exitStatus = cmd.exitStatus
     logd("exampleExec exitStatus $exitStatus")
   }
+
+
+  private fun exampleLocalPF() {
+    val ssh = SSHClient()
+    ssh.addHostKeyVerifier(PromiscuousVerifier())
+    ssh.connect(host)
+    ssh.authPassword(user, password)
+
+    val params = Parameters("127.0.0.1", 8081, "127.0.0.1", 8081)
+
+    val ss = ServerSocket()
+
+    ss.reuseAddress = true
+    ss.bind(InetSocketAddress(params.localHost, params.localPort))
+    ss.use { ss ->
+      ssh.newLocalPortForwarder(params, ss).listen()
+    }
+
+
+  }
+
+
 }
