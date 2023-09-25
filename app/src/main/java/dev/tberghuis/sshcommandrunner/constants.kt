a2f5f67c12992dc003723022934ef3e5861f3bd1
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-09-25 11:26:28 +1000

    .

--- app/src/main/java/dev/tberghuis/sshcommandrunner/constants.kt
+++ app/src/main/java/dev/tberghuis/sshcommandrunner/constants.kt
@@ -3,3 +3,5 @@ package dev.tberghuis.sshcommandrunner
 const val FOREGROUND_SERVICE_NOTIFICATION_ID = 1
 const val NOTIFICATION_CHANNEL = "dev.tberghuis.sshcommandrunner.CHANNEL_SSH_COMMAND_RUNNING"
 const val NOTIFICATION_CHANNEL_DISPLAY = "SSH command running"
+
+const val REQUEST_CODE_PENDING_INTENT = 2
--- app/src/main/java/dev/tberghuis/sshcommandrunner/service/SshService.kt
+++ app/src/main/java/dev/tberghuis/sshcommandrunner/service/SshService.kt
@@ -1,14 +1,19 @@
 package dev.tberghuis.sshcommandrunner.service
 
 import android.app.Notification
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import dev.tberghuis.sshcommandrunner.FOREGROUND_SERVICE_NOTIFICATION_ID
+import dev.tberghuis.sshcommandrunner.MainActivity
 import dev.tberghuis.sshcommandrunner.MyApplication
 import dev.tberghuis.sshcommandrunner.NOTIFICATION_CHANNEL
+import dev.tberghuis.sshcommandrunner.R
+import dev.tberghuis.sshcommandrunner.REQUEST_CODE_PENDING_INTENT
 import dev.tberghuis.sshcommandrunner.tmp2.Command
 import dev.tberghuis.sshcommandrunner.util.logd
 import kotlinx.coroutines.CoroutineScope
@@ -24,6 +29,7 @@ class SshService : Service() {
   private var sshController: SshController? = null
 
   private val binder = LocalBinder()
+
   inner class LocalBinder : Binder() {
     fun getService(): SshService = this@SshService
   }
@@ -81,8 +87,22 @@ class SshService : Service() {
   }
 
   private fun buildNotification(): Notification {
+
+    val pendingIntent: PendingIntent =
+      Intent(this, MainActivity::class.java).let { notificationIntent ->
+        PendingIntent.getActivity(
+          this,
+          REQUEST_CODE_PENDING_INTENT,
+          notificationIntent,
+          FLAG_IMMUTABLE
+        )
+      }
+
+
     val notification: Notification =
       NotificationCompat.Builder(this, NOTIFICATION_CHANNEL).setContentTitle("SSH command running")
+        .setSmallIcon(R.mipmap.ic_launcher)
+        .setContentIntent(pendingIntent)
         .build()
     return notification
   }
